f8d5f3317e0b601d318466eee563a28601c96bde
emiliollbb <emiliollbb@gmail.com>
2022-08-05 07:59:45 +0200

    Add Durango palette color English names

--- inc/video.h
+++ inc/video.h
@@ -9,6 +9,7 @@
 #define SCREEN_3 0x30
 #define RGB 0x08
 #define LED 0x04
+
 #define NEGRO 0x00
 #define VERDE 0x11
 #define ROJO 0x22
@@ -25,6 +26,23 @@
 #define CIAN 0xdd
 #define FUCSIA 0xee
 #define BLANCO 0xff
+
+#define BLACK 0x00
+#define GREEN 0x11
+#define RED 0x22
+#define ORANGE 0x33
+#define PHARMACY_GREEN 0x44
+#define LIME 0x55
+#define MYSTIC_RED 0x66
+#define YELLOW 0x77
+#define BLUE 0x88
+#define DEEP_SKY_BLUE 0x99
+#define MAGENTA 0xaa
+#define LAVENDER_ROSE 0xbb
+#define NAVY_BLUE 0xcc
+#define CIAN 0xdd
+#define PINK_FLAMINGO 0xee
+#define WHITE 0xff
 
 
 typedef struct{
