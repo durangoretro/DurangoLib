02c3d5143ccc85ad32a8c29cb8a4f8855e648401
Emilio <emiliollbb@gmail.com>
2022-08-06 17:11:28 +0200

    Draw rect procedure

--- inc/durango.h
+++ inc/durango.h
@@ -3,12 +3,27 @@
 
 #include "video.h"
 
-extern void __cdecl__ drawPixelPair(unsigned char x, unsigned char y, unsigned char color);
-extern void __fastcall__ waitVsync(void);
-extern void __fastcall__ waitFrames(unsigned short frames);
-extern void __fastcall__ fillScreen(unsigned short color);
-extern void __fastcall__ consoleLogHex(unsigned short value);
-extern void __fastcall__ consoleLogChar(unsigned short value);
+/* Log procedures */
+// Log hex value in emulator
+extern void __fastcall__ consoleLogHex(unsigned char value);
+// Log char value in emulator
+extern void __fastcall__ consoleLogChar(unsigned char value);
+// Log string in emulator
 extern void __fastcall__ consoleLogStr(char *str);
+
+/* Sync procedures */
+// Wait for vsync time
+extern void __fastcall__ waitVsync(void);
+// Wait for n frames
+extern void __fastcall__ waitFrames(unsigned char frames);
+
+/* Draw procedures */
+// Fill screen with solid color
+extern void __fastcall__ fillScreen(unsigned char color);
+// Draw two pixels in screen
+extern void __cdecl__ drawPixelPair(unsigned char x, unsigned char y, unsigned char color);
+// Draw rect in screen
+extern void __cdecl__ drawRect(unsigned char x, unsigned char y, unsigned char width, unsigned char height, unsigned char color);
+
 
 #endif
