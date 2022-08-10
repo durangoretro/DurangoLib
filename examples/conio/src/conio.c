5df440cb46d6316098a78c2942b06df1f2f21acc
Carlos Santisteban <zuiko21@gmail.com>
2022-08-10 19:41:34 +0200

    fix string in conio text example

--- examples/conio/src/conio.c
+++ examples/conio/src/conio.c
@@ -11,7 +11,7 @@
 #include <durango.h>
 
 int main(){
-	char text[10]={12, "Hello, world!", 0};
+	char text[15]={12, 'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', 0};
 	unsigned char i;
 	
 	while (text[i]) {
