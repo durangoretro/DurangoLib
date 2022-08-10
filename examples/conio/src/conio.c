39cc95adbf6703272d9226e5cf54c390d0a63289
emiliollbb <emiliollbb@gmail.com>
2022-08-10 19:30:55 +0200

    Conio example

---
+++ examples/conio/src/conio.c
@@ -0,0 +1,16 @@
+/*
+ * Hello Durango
+ * To manually build:
+ *  Use Makefile after compile the lib:
+ *  cd ../..
+ *  make all
+ *  cd examples/fillscreen
+ *  make
+ */
+
+#include <durango.h>
+
+int main(){
+    
+	return 0;
+}
