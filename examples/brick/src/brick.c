ee7403a43b26df6c8bcf1cfa613a3dfefc572be7
Emilio <emiliollbb@gmail.com>
2022-08-19 17:38:41 +0200

    Brick game

---
+++ examples/brick/src/brick.c
@@ -0,0 +1,25 @@
+/*
+ * Brick game
+ */
+
+#include <durango.h>
+
+unsigned char x, y, gamepad;
+
+void displayTitle() {
+	fillScreen(WHITE);
+	waitStart();
+fillScreen(RED);
+}
+
+int main(){
+	displayTitle();	
+
+	// Draw background color
+	fillScreen(YELLOW);
+
+
+	
+	
+	return 0;
+}
