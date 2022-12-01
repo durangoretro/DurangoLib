556c48b5ba85cb82347de020d1dfeb3a6bd6d27f
Emilio <emiliollbb@gmail.com>
2022-12-01 20:05:05 +0100

    Add get char

--- asm/system.s
+++ asm/system.s
@@ -1,9 +1,18 @@
+; ---------------------------------------------------------------------------
+; @author: Emilio Lopez Berenguer emilio@emiliollbb.net
+; @author: Carlos Santisteban Salinas zuiko21@gmail.com
+; @author: Victor Suárez García zerasul@gmail.com
+; ---------------------------------------------------------------------------
+
+
 .include "durango_hw.inc"
+.include "crt0.inc"
 .PC02
 
 .importzp  sp
 
 .export _setHiRes
+.export _getChar
 
 .proc _setHiRes: near
 	CMP #0
@@ -18,4 +27,11 @@
 	STA VIDEO_MODE
 	end:
 	RTS
+.endproc
+
+.proc _getChar: near
+	read:
+	LDA KEY_PRESSED
+	BEQ read
+	rts
 .endproc
