dd9757a437c22babc421d8657a34edd9190e74ab
zerasul <zerasul@gmail.com>
2023-07-18 13:52:14 +0200

    adding music h file info

--- inc/music.h
+++ inc/music.h
@@ -1,3 +1,12 @@
+/* Copyright 2023 Durango Computer Team
+
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+*/
+
+/** @file music.h Sound Utilities Header File **/
+
 #ifndef MUSIC_H
 #define MUSIC_H
 
